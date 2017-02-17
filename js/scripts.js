b0d3a35907aef5249568e6debcdefc2720c2518a
Xi Xia <tirplextension@gmail.com>
2017-02-17 10:16:41 -0800

    add proof of idea that track score can be updated, considering which question and choice was answered

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,33 @@
 var questionCounter = 1;
+var track1 =0;
+var track2 =0;
 
-var rightTrack = function () {
+var displayTracks = function () {
   $("#yourTracks").show();
   $("#formAnswerSelector").hide();
 }
 
+var adjustTrackScore = function (userAnswer) {
+  if (($("#question"+questionCounter).hasClass("track1"))) {
+    track1 = userAnswer + track1;
+    console.log(track1);
+  }
+}
+
 $(document).ready(function() {
   $("form#answerChoices").submit(function(event) {
-  var someInput = parseInt($("input:radio[name=tendency]:checked").val());
-    if (isNaN(someInput)) {
+  var userAnswer = parseInt($("input:radio[name=tendency]:checked").val());
+    if (isNaN(userAnswer)) {
       alert ("please make a selection");
     } else {
+      //  check wich class of track it has and run function to update global
+      adjustTrackScore (userAnswer);
       $("#question"+questionCounter).hide();
+
       questionCounter++;
       if (questionCounter === 3) {
-        rightTrack ();
+        displayTracks ();
       }
-
 
       $("#question"+questionCounter).show();
       document.getElementById("answerChoices").reset();
