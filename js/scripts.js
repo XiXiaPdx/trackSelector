e36785c8c2ae1ec4203f4ddad175c2489ad92c8d
Xi Xia <tirplextension@gmail.com>
2017-02-17 09:34:50 -0800

    add logic to trigger Track Output after last question

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
 var questionCounter = 1;
 
+var rightTrack = function () {
+  $("#yourTracks").show();
+}
+
 $(document).ready(function() {
   $("form#answerChoices").submit(function(event) {
   var someInput = parseInt($("input:radio[name=tendency]:checked").val());
     if (isNaN(someInput)) {
       alert ("please make a selection");
     } else {
-      debugger;
       $("#question"+questionCounter).hide();
       questionCounter++;
+      if (questionCounter === 3) {
+        rightTrack ();
+      }
+
+
       $("#question"+questionCounter).show();
     }
 
