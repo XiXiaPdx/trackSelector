1f6eac3a752642f15816954f82806729d3c26580
Xi Xia <tirplextension@gmail.com>
2017-02-17 09:23:03 -0800

    add question counter to keep track of which question to hide, which to show

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
+var questionCounter = 1;
+
 $(document).ready(function() {
   $("form#answerChoices").submit(function(event) {
-  var someInput = $("input:radio[name=tendency]:checked").val();
-  console.log(someInput);
+  var someInput = parseInt($("input:radio[name=tendency]:checked").val());
+    if (isNaN(someInput)) {
+      alert ("please make a selection");
+    } else {
+      debugger;
+      $("#question"+questionCounter).hide();
+      questionCounter++;
+      $("#question"+questionCounter).show();
+    }
+
   event.preventDefault();
   });
 
